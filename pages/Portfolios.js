d93070ef05d483c96459e60c52aaff422c6a0fdb
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-12-18 15:40:34 +0900

    dynamic route post id and portfolio page detail

--- pages/Portfolios.js
+++ pages/Portfolios.js
@@ -1,5 +1,6 @@
 import React from "react";
 import BaseLayout from "../components/layouts/BaseLayout";
+import Link from "next/link";
 import axios from "axios";
 
 class Portfolios extends React.Component {
@@ -13,16 +14,22 @@ class Portfolios extends React.Component {
 		}
 		return { posts: posts.slice(0, 10) };
 	}
+
+	renderPosts(posts) {
+		return posts.map((post) => (
+			<li style={{ fontSize: "20px" }} key={post.id}>
+				<Link href={`/Portfolios/[id]`} as={`/Portfolios/${post.id}`}>
+					<a>{post.title}</a>
+				</Link>
+			</li>
+		));
+	}
 	render() {
 		const { posts } = this.props;
 		return (
 			<BaseLayout>
 				<h1>Hello Portfolios</h1>
-				<ul>
-					{posts.map((post) => (
-						<li key={post.id}>{post.title}</li>
-					))}
-				</ul>
+				<ul>{this.renderPosts(posts)}</ul>
 			</BaseLayout>
 		);
 	}
---
+++ pages/Portfolios/[id].js
@@ -0,0 +1,16 @@
+import React from "react";
+import BaseLayout from "../../components/layouts/BaseLayout";
+import { withRouter } from "next/router";
+
+class Portfolio extends React.Component {
+	render() {
+		return (
+			<BaseLayout>
+				<h1>Hello Portfolio page</h1>
+				<h2>{this.props.router.query.id}</h2>
+			</BaseLayout>
+		);
+	}
+}
+
+export default withRouter(Portfolio);
