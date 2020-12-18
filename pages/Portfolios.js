f780974a2ef62051e5f24e128bf0d022cd4ad3f3
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-12-18 13:27:01 +0900

    success pages

---
+++ pages/Portfolios.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Portfolios extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Hello Next</h1>
+			</div>
+		);
+	}
+}
+
+export default Portfolios;
