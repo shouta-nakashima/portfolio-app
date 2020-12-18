6e954909b45f9c3ec789d79dcd00d46980626f1d
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-12-18 15:14:12 +0900

    fetch portfolios data from jsonplacheholder

--- pages/Portfolios.js
+++ pages/Portfolios.js
@@ -1,11 +1,28 @@
 import React from "react";
 import BaseLayout from "../components/layouts/BaseLayout";
+import axios from "axios";
 
 class Portfolios extends React.Component {
+	static async getInitialProps() {
+		let posts = [];
+		try {
+			const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+			posts = res.data;
+		} catch (e) {
+			console.log(e);
+		}
+		return { posts: posts.slice(0, 10) };
+	}
 	render() {
+		const { posts } = this.props;
 		return (
 			<BaseLayout>
 				<h1>Hello Portfolios</h1>
+				<ul>
+					{posts.map((post) => (
+						<li key={post.id}>{post.title}</li>
+					))}
+				</ul>
 			</BaseLayout>
 		);
 	}
